9116cb43f9dd08aed4e8bab03f6491d3b60ef0b8
harsha2002102002 <your_email@example.com>
2025-01-28 10:23:20 +0530

    new

--- lib/views/splashscreen/splashscreen.dart
+++ lib/views/splashscreen/splashscreen.dart
@@ -21,7 +21,7 @@ class SplashScreen extends StatelessWidget {
                 widthFactor: 20.0,
                 heightFactor: 20.0,
                 child: Image.asset(
-                  "assets/images/logo.png",
+                  'packages/vbms/assets/images/logo.png',
                   height: 200,
                 ))
           ],
